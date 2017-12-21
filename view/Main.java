b389557cdffa10dffdf9cffc6c6e9f2503c4c8b9
Amemida <30404149+Amemida@users.noreply.github.com>
2017-12-21 11:06:55 +0800

    Update Main.java

--- view/Main.java
+++ view/Main.java
@@ -3,29 +3,39 @@ package view;
 import java.awt.*;
 
 import javax.swing.*;
+import model.User;
 
 public class Main extends JFrame {
 	
 	private JTabbedPane tab = new JTabbedPane();
+	private JLabel title = new JLabel("图书借阅系统");
+	private JLabel name = new JLabel();
 
-	public Main() {
+	public Main(User user) {
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(800, 600);
-		this.setTitle("图书借阅系统");
+		this.setTitle("鍥句功鍊熼槄绯荤粺");
+		this.setResizable(false);
 		
+		name.setText(user.getName());
 
 		tab.addTab("fucker", new JPanel());
 		tab.addTab("lover", new JPanel());
 		tab.addTab("shitt", new JPanel());
 		
+		Box hbox = new Box(BoxLayout.X_AXIS);
+		title.setFont(new java.awt.Font("楷体", 1, 30));
+		hbox.add(title);
+		hbox.add(Box.createHorizontalGlue());
+		hbox.add(name);
+		
 		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-		add(new Title());
+		add(hbox);
 		add(tab);
 	}
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-		new Main();
+		new Main(new User());
 	}
 
 }
